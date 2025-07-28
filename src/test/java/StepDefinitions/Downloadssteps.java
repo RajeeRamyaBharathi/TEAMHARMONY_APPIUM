4181899b52623af18e508f1a7103c6e40383815f
Reshmi <r.reshmi87@gmail.com>
2025-07-28 09:24:11 -0700

    Added feature file and empty steps and 1 dependency

---
+++ src/test/java/StepDefinitions/Downloadssteps.java
@@ -0,0 +1,57 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Downloadssteps {
+	
+	@Given("The user is already in the trending page of the Skytube application")
+	public void the_user_is_already_in_the_trending_page_of_the_skytube_application() {
+	   
+	    
+	}
+
+	@When("Click on the Downloads tab")
+	public void click_on_the_downloads_tab() {
+	   
+	    
+	}
+
+	@Then("Downloads tab is opened successfully and message Download some videos to find them here is displayed")
+	public void downloads_tab_is_opened_successfully_and_message_download_some_videos_to_find_them_here_is_displayed() {
+	   
+	    
+	}
+
+	@When("User clicks on the Download and Delete Download options from the Trending page of any video")
+	public void user_clicks_on_the_download_and_delete_download_options_from_the_trending_page_of_any_video() {
+	   
+	    
+	}
+
+	@Then("Clicking on Download option from the Trending page adds the Video to the Downloads tab and clicking on Delete Download option from the Trending page removes the video from the Downloads tab")
+	public void clicking_on_download_option_from_the_trending_page_adds_the_video_to_the_downloads_tab_and_clicking_on_delete_download_option_from_the_trending_page_removes_the_video_from_the_downloads_tab() {
+	   
+	    
+	}
+
+	@Given("The user just clicks on a video and plays it")
+	public void the_user_just_clicks_on_a_video_and_plays_it() {
+	   
+	    
+	}
+
+	@When("User clicks on the Download and Delete Download options after playing the video")
+	public void user_clicks_on_the_download_and_delete_download_options_after_playing_the_video() {
+	   
+	    
+	}
+
+	@Then("Clicking on Download option when the video plays adds the Video to the Downloads tab and clicking on Delete Download option when the video is played removes the video from the Downloads tab")
+	public void clicking_on_download_option_when_the_video_plays_adds_the_video_to_the_downloads_tab_and_clicking_on_delete_download_option_when_the_video_is_played_removes_the_video_from_the_downloads_tab() {
+	   
+	    
+	}
+
+}
