b2e976ffae23354a4841544445334b6deb769093
rai123391 <rathih2009@gmail.com>
2025-07-28 20:45:53 -0400

    commit to latest Rathi files

---
+++ src/test/java/StepDefinitions/VideoBlocker.java
@@ -0,0 +1,24 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class VideoBlocker {
+	
+		@Given("The user is on the Preferences tab")
+		public void the_user_is_on_the_preferences_tab() {
+		    
+		}
+
+		@When("The user clicks the Video Blocker button")
+		public void the_user_clicks_the_video_blocker_button() {
+		    		
+		}
+
+		@Then("The user should see Video Blocker button clicked, Channel Filters options, Language and Region Filters, and Other Filters")
+		public void the_user_should_see_video_blocker_button_clicked_channel_filters_options_language_and_region_filters_and_other_filters() {
+	 		
+		}
+
+}
